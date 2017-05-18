2cf821a81fc485c6cea53342a4346a82f6cb651f
cytopia <cytopia@everythingcli.org>
2017-05-18 16:20:19 +0200

    REL-0.9 Fixing travis builds

--- build/docker-validate.sh
+++ build/docker-validate.sh
@@ -327,14 +327,14 @@ print_h1 "[09]   T E S T   F I L E   L O G S"
 
 recreate_dirs
 docker_start "-p 9000 -v ${MY_HTML_DIR}:/var/www/html -e DEBUG_COMPOSE_ENTRYPOINT=${DEBUG} -e DOCKER_LOGS_ERROR=0 -e DOCKER_LOGS_ACCESS=0 -e DOCKER_LOGS_XDEBUG=0"
-docker_start_httpd "-p 80 -v ${MY_HTML_DIR}:/var/www/html -e PHP_FPM_ENABLE=1 -e PHP_FPM_SERVER_ADDR=php -e PHP_FPM_SERVER_PORT=9000 --link php"
+docker_start_httpd "-p 80:80 -v ${MY_HTML_DIR}:/var/www/html -e PHP_FPM_ENABLE=1 -e PHP_FPM_SERVER_ADDR=php -e PHP_FPM_SERVER_PORT=9000 --link ${MY_DOCKER_NAME}"
 
 # Produce PHP error
-echo "echo echo include" > "${MY_HTML_DIR}/index.php"
-run "curl localhost"
-# Check logs
+echo "<?php echo echo include" > "${MY_HTML_DIR}/index.php"
+run "curl localhost | grep 'syntax error'"
+# Check logs (something must be in there)
 docker_exec "ls -lap /var/log/php/"
-docker_exec "find /var/log/php/ -type f -exec cat {} \\;"
+docker_exec "find /var/log/php/ -type f -exec cat {} \\; | grep 'syntax error"
 # Check docker logs
 docker_logs
 
@@ -350,18 +350,16 @@ print_h1 "[10]   T E S T   D O C K E R   L O G S"
 
 recreate_dirs
 docker_start "-p 9000 -v ${MY_HTML_DIR}:/var/www/html -e DEBUG_COMPOSE_ENTRYPOINT=${DEBUG} -e DOCKER_LOGS_ERROR=1 -e DOCKER_LOGS_ACCESS=1 -e DOCKER_LOGS_XDEBUG=1"
-docker_start_httpd "-p 80 -v ${MY_HTML_DIR}:/var/www/html -e PHP_FPM_ENABLE=1 -e PHP_FPM_SERVER_ADDR=php -e PHP_FPM_SERVER_PORT=9000 --link php"
+docker_start_httpd "-p 80:80 -v ${MY_HTML_DIR}:/var/www/html -e PHP_FPM_ENABLE=1 -e PHP_FPM_SERVER_ADDR=php -e PHP_FPM_SERVER_PORT=9000 --link ${MY_DOCKER_NAME}"
 
 # Produce PHP error
-echo "echo echo include" > "${MY_HTML_DIR}/index.php"
-run "curl localhost"
-# Check logs
+echo "<?php echo echo include" > "${MY_HTML_DIR}/index.php"
+run "curl localhost | grep 'syntax error'"
+# Check logs (nothing should be in there)
 docker_exec "ls -lap /var/log/php/"
-docker_exec "find /var/log/php/ -type f -exec cat {} \\;"
+docker_exec_false "find /var/log/php/ -type f -exec cat {} \\; | grep 'syntax error'"
 # Check docker logs
 docker_logs
 
 docker_stop_httpd
 docker_stop
-
-
